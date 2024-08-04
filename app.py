86c1ddb3fe129f092a59c1d687580b7278a6e38d
angelof-exe <angelo99franciamore@gmail.com>
2024-08-04 19:16:28 +0200

    feat: le puntine sulle mappe sono colorate a seconda del loro stato

--- app.py
+++ app.py
@@ -1,31 +1,44 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import requests
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 app = Flask(__name__)
 
-GOOGLE_MAPS_API_KEY = os.getenv('API_KEY')  
-MAP_ID = os.getenv('MAP_ID')
+def fetch_watering_places():
+    overpass_url = "http://overpass-api.de/api/interpreter"
+    overpass_query = """
+    [out:json][timeout:25];
+    nwr["amenity"="watering_place"](36.3203615818532,12.008056640625,39.15214258358986,16.644287109375004);
+    out geom;
+    """
+    response = requests.get(overpass_url, params={'data': overpass_query})
+    data = response.json()
+    
+    watering_places = []
+    for element in data['elements']:
+        lat = element.get('lat')
+        lon = element.get('lon')
+        
+        if not lat or not lon:
+            if 'center' in element:
+                lat = element['center'].get('lat')
+                lon = element['center'].get('lon')
 
-def get_water_troughs(location='37.5999948,13.7189441', radius=50000, keyword='abbeveratoio'):
-    url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json"
-    params = {
-        'location': location,
-        'radius': radius,
-        'keyword': keyword,
-        'key': GOOGLE_MAPS_API_KEY
-    }
-    response = requests.get(url, params=params)
-    results = response.json().get('results', [])
-    return results
+        if lat and lon:  # Ensure we have valid coordinates before adding to list
+            place = {
+                'id': element['id'],
+                'lat': lat,
+                'lon': lon,
+                'tags': element['tags'],
+                'status': element['tags'].get('status', 'unknown')
+            }
+            watering_places.append(place)
+    
+    return watering_places
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    water_troughs = get_water_troughs()
-    return render_template('index.html', api_key=GOOGLE_MAPS_API_KEY, map_id=MAP_ID, water_troughs=water_troughs)
+    watering_places = fetch_watering_places()
+    return render_template('index.html', watering_places=watering_places)
 
 if __name__ == '__main__':
     app.run(debug=True)
